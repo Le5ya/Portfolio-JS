c8a440b149ec2ecb406df2343b7e41652e7287e5
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-03-21 10:40:52 +0300

    fixing declension of numbers

--- js/timer.js
+++ js/timer.js
@@ -3,6 +3,19 @@ const deadline = '31 march 2022'
 
 let interval
 
+wordForm = function (days, words) {
+  words = ['день', 'дня', 'дней'];
+  days=Math.abs(days) % 100;
+  let n = days % 10;
+  if(days > 10 && days < 20)return words[2];
+  if(n > 1 && n < 5) return words[1];
+  if(n ===1) return words[0];
+  return words[2];
+  
+}
+
+
+
 const updateClock = () => {
   const date = new Date().getTime()
   const dateDeadline = new Date(deadline).getTime()
@@ -13,7 +26,11 @@ const updateClock = () => {
   const minutes = Math.floor((timeRemaining / 60) % 60)
   const seconds = Math.floor(timeRemaining % 60)
 
-  const fDays = days >= 5 || days < 1 ? days + ' дней'  : days >= 2 ?  days + ' дня' : days + ' день'
+  // const fDays = days >= 5 || days < 1 ? days + ' дней'  : days >= 2 ?  days + ' дня' : days + ' день'
+
+  
+ 
+  const fDays = days + ' ' + wordForm(days)
   const fHours = hours < 10 ? '0' + hours : hours
   const fMinutes = minutes < 10 ? '0' + minutes : minutes
   const fSeconds = seconds < 10 ? '0' + seconds : seconds
